a02169386bc811512c781e9a9e15098c8d042421
Cristin O'Connor <her@cristin.io>
2021-03-20 00:30:48 -0600

    My code for completed lesson 08: https://www.vuemastery.com/courses/vue3-typescript/computed-\&-methods-with-custom-types

---
+++ src/components/CustomTypesComputed.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+// eslint-disable-next-line no-unused-vars
+import { EventItem } from '@/types'
+
+export default defineComponent({
+  data() {
+    return {
+      events: [] as EventItem[]
+    }
+  },
+  computed: {
+    firstEvent(): EventItem {
+      return this.events[0]
+    }
+  },
+  methods: {
+    addEvent(newEvent: EventItem) {
+      this.events.push(newEvent)
+    },
+    secondEvent(): EventItem {
+      return this.events[1]
+    }
+  }
+})
+</script>
